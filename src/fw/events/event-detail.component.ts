e52532f013df2678165da3c32f51079826201407
Martin Ivanov <martin.ivanov@sircleadvertising.com>
2018-05-13 15:39:23 +0300

    Final fixes and the presentation

--- src/fw/events/event-detail.component.ts
+++ src/fw/events/event-detail.component.ts
@@ -1,7 +1,7 @@
-import {Component, OnInit} from '@angular/core';
-import {IEvent, IFanFeed, ILiveFeed} from './event';
-import {Socket} from 'ng-socket-io';
-import {ActivatedRoute} from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { IEvent, IFanFeed, ILiveFeed } from './event';
+import { Socket } from 'ng-socket-io';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'fw-event-detail',
@@ -20,44 +20,55 @@ export class EventDetailComponent implements OnInit {
     };
 
 
-  eventLiveFeeds: ILiveFeed[] = [{
+  eventLiveFeeds: ILiveFeed[] = [
+    {
       "feedType": "GOAL!!!",
-      "feedImage": "../assets/images/goal.jpg",
-      'likes' : 100,
+      "feedImage": "../assets/images/city.jpg",
+      'likes': 233,
     },
     {
-      "feedType": "WIN",
-      "feedImage": "../assets/images/win.jpg",
-      'likes' : 100,
-    }
-];
-
-// eventFanFeeds: IFanFeed[] = [{
-//   "feedType": "GOAL!!!",
-// "feedImage": "../assets/images/goal.jpg",
-//   "likes": 20,
-// },
-// {
-//   "feedType": "WIN",
-//   "feedImage": "../assets/images/win.jpg",
-//   "likes": 20,
-// }
-// ];
-
-constructor(private socket: Socket, private route: ActivatedRoute)
-{
-}
-
- ngOnInit(): void {
-  this.socket.on('event', (e) => {
-  if (e.nationalTeam && e.nationalTeam.id === this.route.snapshot.paramMap.get('id') && e.type === 'GOAL') {
-    console.log(e);
-    let event = this.events.find(({eventName}) => e.nationalTeam.name === eventName);
-    if (event) {
-      event.eventName = `${e.player.firstName} ${e.player.lastName} SCORED`;
-      //console.log(scoringClub.event);
-    }
+      "feedType": "Coaches agree it was a good game.",
+      "feedImage": "../assets/images/city2.jpg",
+      'likes': 95,
+    },{
+    "feedType": "GOAL!!!",
+    "feedImage": "../assets/images/goal.jpg",
+    'likes': 70,
+  },
+  {
+    "feedType": "WIN",
+    "feedImage": "../assets/images/win.jpg",
+    'likes': 56,
   }
-});
-}
+  ];
+
+  eventFanFeeds: IFanFeed[] = [{
+    "feedType": "GOAL!!!",
+    "feedImage": "../assets/images/miss1.png",
+    "likes": 503,
+    "points" : 192
+  },
+  {
+    "feedType": "WIN",
+    "feedImage": "../assets/images/miss2.png",
+    "likes": 209,
+    "points" : 342
+  }
+  ];
+
+  constructor(private socket: Socket, private route: ActivatedRoute) {
+  }
+
+  ngOnInit(): void {
+    this.socket.on('event', (e) => {
+      if (e.nationalTeam && e.nationalTeam.id === this.route.snapshot.paramMap.get('id') && e.type === 'GOAL') {
+        console.log(e);
+        let event = this.events.find(({ eventName }) => e.nationalTeam.name === eventName);
+        if (event) {
+          event.eventName = `${e.player.firstName} ${e.player.lastName} SCORED`;
+          //console.log(scoringClub.event);
+        }
+      }
+    });
+  }
 }
